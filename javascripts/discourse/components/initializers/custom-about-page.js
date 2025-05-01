639e6e7ce7649af3f2a335c3e5fb9a083dbe0d86
kaylieryan <kayliemryan@gmail.com>
2025-05-01 12:57:51 +0100

    Properly set up initializer.

--- javascripts/discourse/components/initializers/custom-about-page.js
+++ javascripts/discourse/components/initializers/custom-about-page.js
@@ -2,7 +2,7 @@ import { apiInitializer } from "discourse/lib/api";
 import CustomAboutAdmins from "../custom-about-admins";
 import CustomAboutModerators from "../custom-about-moderators";
 
-export default apiInitializer((api) => {
+export default apiInitializer("1.13.0", (api) => {
   // Render our custom admins list into the about-after-admins outlet
   api.renderInOutlet("about-after-admins", CustomAboutAdmins);
   
