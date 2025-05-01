8571669597824b120998ada7077bdbcf561ed2df
kaylieryan <kayliemryan@gmail.com>
2025-05-01 11:12:01 +0100

    update imports and api calls from new wrapper files.

--- javascripts/discourse/components/initializers/custom-about-page.js
+++ javascripts/discourse/components/initializers/custom-about-page.js
@@ -1,10 +1,11 @@
 import { apiInitializer } from "discourse/lib/api";
-import CustomAboutWrapper from "../custom-about-wrapper";
+import CustomAboutAdmins from "../custom-about-admins";
+import CustomAboutModerators from "../custom-about-moderators";
 
 export default apiInitializer((api) => {
   // Render our custom admins list into the about-after-admins outlet
-  api.renderInOutlet("about-after-admins", CustomAboutWrapper);
+  api.renderInOutlet("about-after-admins", CustomAboutAdmins);
   
   // Render our custom moderators list into the about-after-moderators outlet
-  api.renderInOutlet("about-after-moderators", CustomAboutWrapper);
+  api.renderInOutlet("about-after-moderators", CustomAboutModerators);
 }); 
